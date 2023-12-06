7253ad99d7943f5623f0c1a0bacafa733b7c5a89
Karina Aulia <Karina Aulia>
2023-12-06 16:23:37 +0700

    [Created] : Add SplashScreen Page

--- app/src/main/java/com/bangkit/navomobility/MainActivity.kt
+++ app/src/main/java/com/bangkit/navomobility/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bangkit.navomobility
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bangkit.navomobility.ui.screen.presentation.onboarding.OnBoardingScreen
 import com.bangkit.navomobility.ui.theme.NavoMobilityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NavoMobilityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                OnBoardingScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavoMobilityTheme {
-        Greeting("Android")
     }
 }
