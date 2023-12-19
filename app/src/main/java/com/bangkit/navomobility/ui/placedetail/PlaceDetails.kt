3877eb14550b84b92643f6d5b67e9e25fc5949b7
jidan18jidan <jidan18jidan@apps.ipb.ac.id>
2023-12-19 10:39:22 +0700

    delete

--- app/src/main/java/com/bangkit/navomobility/ui/placedetail/PlaceDetails.kt
+++ app/src/main/java/com/bangkit/navomobility/ui/placedetail/PlaceDetails.kt
@@ -16,11 +16,13 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 import com.bangkit.navomobility.R
